215faf5a81450aaf211f7ff0ec1d1ab53b2a1aca
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-20 20:44:28 +0300

    remove junk, cleanup

--- config.mk
+++ config.mk
@@ -1,8 +1,6 @@
 # project metadata
 NAME        = mkrootfs
 VERSION     = 1.2
-HOMEPAGE    = https://github.com/zeppe-lin/mkrootfs/
-BUGTRACKER  = https://github.com/zeppe-lin/mkrootfs/issues/
 DIST        = ${NAME}-${VERSION}
 
 # paths
